ad939540bad5d9559092681cead4596518a89a95
annisaairinadita <annisaairinadita@gmail.com>
2024-10-02 21:21:49 +0700

    ubah path data

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,8 +3,8 @@ import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-day_df = pd.read_csv('D:\submission\dashboard\data_bersih_day.csv')
-hour_df = pd.read_csv('D:\submission\dashboard\data_bersih_hour.csv')
+day_df = pd.read_csv('https://raw.githubusercontent.com/annisaairinadita/Proyek_Belajar_Analisis_Data/refs/heads/main/dashboard/data_bersih_day.csv')
+hour_df = pd.read_csv('https://raw.githubusercontent.com/annisaairinadita/Proyek_Belajar_Analisis_Data/refs/heads/main/dashboard/data_bersih_hour.csv')
 
 st.title("Bike Sharing Data Analysis Dashboard :bicyclist:")
 st.subheader("Analisis Data Penyewaan Sepeda")
